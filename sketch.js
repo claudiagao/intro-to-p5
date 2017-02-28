891d035497af706bcc7f29bdff4383141ccfd669
cgao <cgao@wlapcs.org>
2017-02-28 20:58:31 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup(){
+    createCanvas(600,400);
+    background(19,183,216);
+}
+
+function draw(){
+    fill(103,30,140);
+    stroke(127,6,119);
+    rect(290,100,250,250);
+    
+    line(200, 200, 400, 500);
+        
+    stroke(255,255,255);
+    fill(132,0,26);
+    ellipse(50,250,80,80);
+    
+    stroke(0,0,0);
+    strokeWeight(4);
+    line(300, 60, 20, 400);
+    
+    line(300,400,300,250);
+    
+    stroke(195,215,216);
+    fill(193,65,191);
+    rect(50,60,150,120);
+    
+    fill(255,127,184);
+    stroke(0,0,0);
+    ellipse(200,300,100,100);
+    
+    line(600,300,20,300);
+    
+}
